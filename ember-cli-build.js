cfcb2eacc7509b725f786a172a3edf997f81a9fe
Travis <travis@travis-ci.org>
2016-07-31 17:26:07 +0700

    Add Semantic UI

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,7 +2,7 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
@@ -19,6 +19,34 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+  // add Semantic UI
+
+  app.import({
+    development: 'bower_components/semantic/dist/semantic.js',
+    production: 'bower_components/semantic/dist/semantic.min.js'
+  });
+
+  app.import({
+    development: 'bower_components/semantic/dist/semantic.css',
+    production: 'bower_components/semantic/dist/semantic.min.css'
+  });
+
+  const semanticAssert = [
+    'fonts/icons.eot',
+    'fonts/icons.otf',
+    'fonts/icons.svg',
+    'fonts/icons.ttf',
+    'fonts/icons.woff',
+    'fonts/icons.woff2',
+    'images/flags.png'
+  ];
+
+  semanticAssert.forEach(asset => {
+    app.import(`bower_components/semantic/dist/themes/default/assets/${asset}`, {
+      destDir: 'assets/themes/default/assets/fonts'
+    });
+  });
 
   return app.toTree();
 };
