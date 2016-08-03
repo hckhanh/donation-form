3f62ba07171f1b571a41d3924b98b7da2a5c12ea
Khanh Hoang <hoangchaukhanh93@gmail.com>
2016-08-03 22:18:48 +0700

    Replace script deploy with after_success

--- .deploy.sh
+++ .deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ember build --env production
-
-npm install -g firebase-tools
-
-firebase deploy --token "$FIREBASE_TOKEN" --message "$TRAVIS_TAG"
+if [[ ! -z "$TRAVIS_TAG" ]]; then
+  ember build --env production
+  npm install -g firebase-tools
+  firebase deploy --token "$FIREBASE_TOKEN" --message "$TRAVIS_TAG"
+fi
