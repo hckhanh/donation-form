d4341f1c849ffd0e209228cfeb8f801a624f9844
Khanh Hoang <hoangchaukhanh93@gmail.com>
2016-08-03 21:58:58 +0700

    Update envs in .deploy.sh

--- .deploy.sh
+++ .deploy.sh
@@ -4,4 +4,4 @@ ember build --env production
 
 npm install -g firebase-tools
 
-firebase deploy --token $FIREBASE_TOKEN --message $TRAVIS_TAG
+firebase deploy --token "$FIREBASE_TOKEN" --message "$TRAVIS_TAG"
