a2dc75df2a6061282587f8af7477a2b262e74c76
Hoang Chau Khanh <Khanh.HoangChau@vn.bosch.com>
2016-08-04 15:48:20 +0700

    Add test for application route #4

--- tests/unit/routes/application-test.js
+++ tests/unit/routes/application-test.js
@@ -1,11 +1,22 @@
+import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:application', 'Unit | Route | application', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: [
+    'adapter:release',
+    'serializer:release',
+    'model:release'
+  ]
 });
 
-test('it exists', function(assert) {
-  let route = this.subject();
-  assert.ok(route);
+test('should get data of the latest release from GitHub', function (assert) {
+  const route = this.subject();
+
+  Ember.run(() => {
+    route.model().then((release) => {
+      assert.equal(release.get('id'), 'latest', 'id must be "latest"');
+      assert.ok(release.get('tag'), 'tag is not null');
+    });
+  });
 });
